f9dca2f456f682e3cdb40c62916956035d9c3590
Adam Coard <arcoard+github@gmail.com>
2020-11-09 12:03:26 -0800

    Load testing working, websocket+HTTP, w/ scenarios
    Result from running:
    
    ```
    All virtual users finished
    Summary report @ 12:02:42(-0800) 2020-11-09
      Scenarios launched:  619
      Scenarios completed: 618
      Requests completed:  3953
      Mean response/sec: 28.49
      Response time (msec):
        min: 0
        max: 30051.4
        median: 3412
        p95: 11993.4
        p99: 17865.3
      Scenario counts:
        CSR – Login, load data, idle: 200 (32.31%)
        CSR - Websockets: 206 (33.279%)
        CSR – Create and delete appointments: 213 (34.41%)
      Codes:
        0: 412
        200: 2690
        201: 425
        204: 425
        504: 1
    ```

--- tests/loadtesting/functions.js
+++ tests/loadtesting/functions.js
@@ -1,13 +1,8 @@
 
 const fetch = require('node-fetch');
 
-QUEUE_URI = 'https://dev-theq.pathfinder.gov.bc.ca/';
 KEYCLOAK_URI = 'https://dev.oidc.gov.bc.ca/auth'
-// TODO - Get this from API call, make fn
-// REFRESH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlNGQwY2YzNS1jYTVkLTQxM2YtYjY2ZC01NzI3YmE2OTdlMmEifQ.eyJleHAiOjE2MDMxMzg2MTYsImlhdCI6MTYwMzEzNjgxNiwianRpIjoiODQ3NjAzODktMTg5YS00YzU0LTgxNTYtOTM3ZjMwN2Y2ZWViIiwiaXNzIjoiaHR0cHM6Ly9kZXYub2lkYy5nb3YuYmMuY2EvYXV0aC9yZWFsbXMvdnRrYXlxNGMiLCJhdWQiOiJodHRwczovL2Rldi5vaWRjLmdvdi5iYy5jYS9hdXRoL3JlYWxtcy92dGtheXE0YyIsInN1YiI6IjhhZDY2YzAzLTA0NWQtNDFkZi04NjAxLWFiMmM2OGUyMTc3MyIsInR5cCI6IlJlZnJlc2giLCJhenAiOiJjZm1zLWRldi1zdGFmZiIsIm5vbmNlIjoiNjA4ODAxMTUtMDI0NC00NzZmLTg5N2MtNzQ4MjJhNjA4OTI0Iiwic2Vzc2lvbl9zdGF0ZSI6ImI5MjdhMDY2LTY4OGItNDlmMi1hNmFkLTJlM2U4YjgxYjQ5OCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ.4HZ6FjGeXM97u-LOOyoMO-eP_hxhttw34yp-S69eRgQ'
-// REFRESH_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlNGQwY2YzNS1jYTVkLTQxM2YtYjY2ZC01NzI3YmE2OTdlMmEifQ.eyJleHAiOjE2MDMxNDAxMTcsImlhdCI6MTYwMzEzODMxNywianRpIjoiZDdjYWQzOGItZTY1ZS00Y2E3LTgyNTctY2MyNjhhNGMzMjJjIiwiaXNzIjoiaHR0cHM6Ly9kZXYub2lkYy5nb3YuYmMuY2EvYXV0aC9yZWFsbXMvdnRrYXlxNGMiLCJhdWQiOiJodHRwczovL2Rldi5vaWRjLmdvdi5iYy5jYS9hdXRoL3JlYWxtcy92dGtheXE0YyIsInN1YiI6IjhhZDY2YzAzLTA0NWQtNDFkZi04NjAxLWFiMmM2OGUyMTc3MyIsInR5cCI6IlJlZnJlc2giLCJhenAiOiJjZm1zLWRldi1zdGFmZiIsIm5vbmNlIjoiNjA4ODAxMTUtMDI0NC00NzZmLTg5N2MtNzQ4MjJhNjA4OTI0Iiwic2Vzc2lvbl9zdGF0ZSI6ImI5MjdhMDY2LTY4OGItNDlmMi1hNmFkLTJlM2U4YjgxYjQ5OCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwifQ._vyD7p7hxayPFzbwDaDs0CFtDb7BeqBGAdpnhzoQnEw'
-
-KEYCLOAK_CLIENT = 'TODO'
+KEYCLOAK_CLIENT = 'cfms-dev-staff'
 
 // {
 //     "realm": "vtkayq4c",
@@ -41,8 +36,7 @@ async function loginToKeycloak(username, password) {
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-site"
         },
-        "body": `grant_type=password&username=${username}&password=${password}&client_id=cfms-dev-staff`,
-        // "body": `grant_type=password&username=admin&password=admin&client_id=cfms-dev-staff`,
+        "body": `grant_type=password&username=${username}&password=${password}&client_id=${KEYCLOAK_CLIENT}`,
         "method": "POST",
         "mode": "cors",
         "credentials": "include"
@@ -57,7 +51,6 @@ async function setAuthHeader(requestParams, context, ee, next) {
     requestParams.headers.Authorization = `Bearer ${access_token}`
     // Websocket requests use oidc-jwt cookie
     requestParams.headers.cookie = `oidc-jwt=${access_token}`
-    // console.log('setAuthHeader end', {requestParams})
     next();
 }
 
@@ -68,7 +61,7 @@ async function setAuthHeader(requestParams, context, ee, next) {
         console.log('SHOULD NOT SEE THIS WHEN RUNNING TESTS');
         console.log('SHOULD see this when running functions directly')
         const {access_token} = await loginToKeycloak('admin', 'admin');
-        console.log('\n\getAccessToken Result:', {access_token})
+        console.log('\n\getAccessToken Result:\n\n', access_token)
     }
 })();
 
